c322a78c918bbcf20463da23546c89f8eeaeec2d
fanyetu <hnan187@126.com>
2017-06-15 23:15:07 +0800

    commit

--- front-vue/ele-demo/src/router/index.js
+++ front-vue/ele-demo/src/router/index.js
@@ -6,23 +6,25 @@ import ratings from 'components/ratings/ratings'
 import seller from 'components/seller/seller'
 
 // 使用router插件
-Vue.use(Router)
+Vue.use(Router);
 
-var router = new Router({
-  routes: [{
-    path:'/goods',
-    component:goods
-  },{
-    path:'/ratings',
-    component:ratings
-  },{
-    path:'/seller',
-    component:seller
-  }],
-  linkActiveClass:'active'
-})
+const routes = [{
+  path: '/',
+  redirect: '/goods'//使用redirect保证进入就跳转到goods组件
+}, {
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}];
 
-//默认跳转到goods页面
-router.push('/goods')
+const router = new Router({
+  linkActiveClass: 'active',
+  routes
+});
 
 export default router
