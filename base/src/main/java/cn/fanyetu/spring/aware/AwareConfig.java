f06ac064175d48a22ec0520e5dc795bf6eb6c744
fanyetu <hnan187@126.com>
2017-06-25 21:18:10 +0800

    commit

--- base/src/main/java/cn/fanyetu/spring/aware/AwareConfig.java
+++ base/src/main/java/cn/fanyetu/spring/aware/AwareConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * java ≈‰÷√¿‡
+ * java ÈÖçÁΩÆÁ±ª
  * @author zhanghaonan
  *
  */
