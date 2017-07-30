c92881335ceba82e82ce6645539f07c6fe25ae43
fanyetu <hnan187@126.com>
2017-07-31 00:30:48 +0800

    commit

---
+++ front-vue/vue-music/src/base/listview/listview.vue
@@ -0,0 +1,64 @@
+<!-- create by zhanghaonan 2017/7/31 -->
+<template>
+  <scroll class="listview" :data="data"><!--将listview的data属性传入scroll组件-->
+    <ul>
+      <li v-for="group in data" class="list-group">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" class="list-group-item">
+            <img v-lazy="item.avatar" class="avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      Scroll
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .listview
+    position relative
+    width 100%
+    height 100%
+    overflow hidden
+    background $color-background
+    .list-group
+      padding-bottom 3px
+      .list-group-title
+        height 30px
+        line-height 30px
+        padding-left 20px
+        font-size $font-size-small
+        color $color-text-l
+        background $color-highlight-background
+      .list-group-item
+        display flex
+        align-items center
+        padding 20px 0 0 30px
+        &:last-child
+          padding-bottom 20px
+        .avatar
+          width 50px
+          height 50px
+          border-radius 50%
+        .name
+          margin-left 20px
+          color $color-text-l
+          font-size $font-size-medium
+</style>
