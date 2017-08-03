4a5e0154a6dee3f6667e0793b7bb4886e0989ef6
fanyetu <hnan187@126.com>
2017-08-03 23:45:40 +0800

    commit

--- front-vue/vue-music/src/components/music-list/music-list.vue
+++ front-vue/vue-music/src/components/music-list/music-list.vue
@@ -9,7 +9,9 @@
       <div class="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div><!--跟随scroll移动的层-->
-    <scroll :data="songs" class="list" ref="list">
+    <!--监听scroll组件的scroll事件-->
+    <scroll :data="songs" class="list" ref="list" @scroll="scroll"
+            :probe-type="probeType" :listen-scroll="listenScroll">
       <div class="song-list-wrapper"><!--控制内部样式-->
         <song-list :songs="songs"></song-list>
       </div>
@@ -19,6 +21,8 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+
+  const RESERVED_HEIGHT = 40
 
   export default {
     props: {
@@ -35,12 +39,49 @@
         default: ''
       }
     },
+    data() {
+      return {
+        scrollY: 0
+      }
+    },
+    created() {
+      this.probeType = 3
+      this.listenScroll = true
+    },
     mounted() {
-      this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
     computed: {
       bgStyle() {
         return `background-image:url(${this.bgImage})`
+      }
+    },
+    methods: {
+      scroll(pos) {
+        this.scrollY = pos.y
+      }
+    },
+    watch: {
+      // 观察scrollY的变化
+      scrollY: function (newY) {
+        let translateY = Math.max(this.minTranslateY, newY)
+        let zIndex = 0
+
+        // 动态改变layer层的transform属性
+        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+        this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style['paddingTop'] = 0
+          this.$refs.bgImage.style['height'] = `${RESERVED_HEIGHT}px`
+        } else {
+          this.$refs.bgImage.style['paddingTop'] = '70%'
+          this.$refs.bgImage.style['height'] = 0
+        }
+        this.$refs.bgImage.style['zIndex'] = zIndex
       }
     },
     components: {
@@ -82,6 +123,10 @@
       line-height 40px
       font-size $font-size-large
       color $color-text
+    .bg-layer
+      position relative
+      height 100%
+      background $color-background
     .bg-image
       position relative
       width 100%
@@ -98,5 +143,6 @@
       background $color-background
       .song-list-wrapper
         padding 20px 30px
+
   /*.test*/
 </style>
