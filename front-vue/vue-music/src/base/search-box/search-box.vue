46c53292d441f04980b1e90214e86ad8f67019b5
fanyetu <hnan187@126.com>
2017-08-22 23:17:47 +0800

    commit

---
+++ front-vue/vue-music/src/base/search-box/search-box.vue
@@ -0,0 +1,11 @@
+<!-- create by zhanghaonan 2017/8/22 -->
+<template>
+  <div>
+    搜索框组件
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+<style lang="stylus">
+</style>
