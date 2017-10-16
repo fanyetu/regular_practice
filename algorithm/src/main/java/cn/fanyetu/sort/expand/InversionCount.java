24d880afe4b221faf0b71b7acaa58584affa2a2b
fanyetu <qaz0987123>
2017-10-16 23:29:08 +0800

    commit

---
+++ algorithm/src/main/java/cn/fanyetu/sort/expand/InversionCount.java
@@ -0,0 +1,9 @@
+package cn.fanyetu.sort.expand;
+
+/**
+ * 求一个数组的逆序对的数量
+ * <p>
+ * 使用归并排序的思想
+ */
+public class InversionCount {
+}
---
+++ algorithm/src/main/java/cn/fanyetu/sort/expand/Selection.java
@@ -0,0 +1,9 @@
+package cn.fanyetu.sort.expand;
+
+/**
+ * 求一个数组中第n大的元素
+ * <p>
+ * 使用快速排序的思想
+ */
+public class Selection {
+}
