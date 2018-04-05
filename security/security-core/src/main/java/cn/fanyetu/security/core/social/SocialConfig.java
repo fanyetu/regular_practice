edf510ab99fd72ba6728b6b6b8e565615d551901
fanyetu <hnan187@126.com>
2018-04-05 23:40:58 +0800

    commit

--- security/security-core/src/main/java/cn/fanyetu/security/core/social/SocialConfig.java
+++ security/security-core/src/main/java/cn/fanyetu/security/core/social/SocialConfig.java
@@ -24,6 +24,23 @@ public class SocialConfig extends SocialConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    /**
+     *   create table UserConnection (userId varchar(255) not null,
+         providerId varchar(255) not null,
+         providerUserId varchar(255),
+         rank int not null,
+         displayName varchar(255),
+         profileUrl varchar(512),
+         imageUrl varchar(512),
+         accessToken varchar(512) not null,
+         secret varchar(512),
+         refreshToken varchar(512),
+         expireTime bigint,
+         primary key (userId, providerId, providerUserId));
+         create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
+     * @param connectionFactoryLocator
+     * @return
+     */
     @Override
     public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
         // 生成基于数据库的connection repository，设置加密方式为不加密
