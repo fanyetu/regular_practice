0760628a8557110e002dea2e62d206a14c610030
fanyetu <hnan187@126.com>
2018-07-11 21:39:24 +0800

    Save before ejecting

--- hadoop/src/test/java/cn/fanyetu/hadoop/hdfs/HDFSTest.java
+++ hadoop/src/test/java/cn/fanyetu/hadoop/hdfs/HDFSTest.java
@@ -1,8 +1,11 @@
 package cn.fanyetu.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,10 +22,31 @@ public class HDFSTest {
 
     public static final String HDFS_PATH = "hdfs://192.168.200.128:8020";
 
-    FileSystem fileSystem = null;
-    Configuration configuration = null;
+    private FileSystem fileSystem = null;
+    private Configuration configuration = null;
+
+    /**
+     * 读取文件
+     * @throws Exception
+     */
+    @Test
+    public void cat() throws Exception{
+        FSDataInputStream in = fileSystem.open(new Path("/hdfsapi/test/a.txt"));
+        IOUtils.copyBytes(in, System.out, 1024);
+    }
 
 
+    /**
+     * 创建文件
+     * @throws Exception
+     */
+    @Test
+    public void create() throws Exception{
+        FSDataOutputStream out = fileSystem.create(new Path("/hdfsapi/test/a.txt"));
+        out.write("hello hadoop".getBytes());
+        out.flush();
+        out.close();
+    }
 
     /**
      * 创建文件夹
